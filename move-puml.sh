5a5224dfe9c4c8ce5c9d94bf4e7875799f80ddd9
anoff <offenhaeuser@gmail.com>
2019-02-21 22:43:37 +0100

    :construction_worker: build: build with plantUML support (hacky)

---
+++ move-puml.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+sourcedir=$(find . -type d -name "temp_image" | head -n 1)
+targetdir=$(find . -type d -name "_images" | head -n 1)
+
+if [ -z "$sourcedir" ] || [ -z "$targetdir" ]
+then
+  exit 0
+fi
+
+# echo "$sourcedir -> $targetdir"
+
+mv $sourcedir/_images/** $targetdir
+
+rmdir $sourcedir/_images
+rmdir $sourcedir
